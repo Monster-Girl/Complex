c878e33f5cd28011d383368166507f86f662e646
Monster-Girl <1104306242@qq.com>
2017-07-29 22:58:08 +0800

    ping的实现（修正版）

--- ping/ping.c
+++ ping/ping.c
@@ -1,224 +1,341 @@
-#include"ping.h"
+/*************************************************************************
+	> File Name: ping.c
+	> Author: monster
+	> Mail: 1104306242@qq.com 
+	> Created Time: Fri 28 Jul 2017 04:53:00 PM CST
+ ************************************************************************/
+#include <sys/socket.h>  
+#include <netinet/in.h>  
+#include <netinet/ip.h>  
+#include <netinet/ip_icmp.h>  
+#include <unistd.h>  
+#include <signal.h>  
+#include <arpa/inet.h>  
+#include <errno.h>  
+#include <sys/time.h>  
+#include <stdio.h>  
+#include <string.h> /* bzero */  
+#include <netdb.h>  
+#include <pthread.h> 
+//保存发送包的状态值  
+typedef struct pingm_packet
+{  
+	struct timeval tv_begin;     //发送时间  
+	struct timeval tv_end;       //接收到的时间  
+	short seq;                   //序列号  
+	int flag;          //1，表示已经发送但是没有接收到回应，0，表示接收到回应  
+}pingm_packet;  
 
-void statistics()
+static pingm_packet *icmp_findpacket(int seq);  
+static unsigned short icmp_cksum(unsigned char *data, int len);  
+static struct timeval icmp_tvsub(struct timeval end, struct timeval begin);  
+static void icmp_statistics();  
+static void icmp_pack(struct icmp *icmph, int seq, struct timeval *tv,int length);  				
+static int icmp_unpack(char *buf,int len); 
+static void *icmp_send();
+static void *icmp_recv();  
+static void icmp_sigint(int signo); 
+static void icmp_usage();  
+
+
+static pingm_packet pingpacket[128];  
+#define K 1024  
+#define BUFFERSIZE 72                            //发送缓冲区的大小  
+static unsigned char send_buff[BUFFERSIZE];        
+static unsigned char recv_buff[2*K];             //防止接收溢出，设置大一些  
+static struct sockaddr_in dest;                  //目的地址  
+static int rawsock = 0;                          //发送和接收线程需要的socket描述符  
+static pid_t pid;                                //进程PID  
+static int alive = 0;                            //是否接收到退出信号  
+static short packet_send = 0;                    //已经发送的数据包数量  			
+static short packet_recv = 0;                    //已经接收的数据包数量  			
+static char dest_str[80];                        //目的主机字符串  
+static struct timeval tv_begin, tv_end, tv_interval; 
+
+
+
+
+void icmp_statistics()
 {
-	printf("\n---%s ping statistics---\n",inet_ntoa(dst_addr.sin_addr));
-	printf("%d packets transmittede, %d received, %.2f%c packet loss\n",\
-			nsend_pkt,nrecv_pkt,\
-			(float)100*(nsend_pkt-nrecv_pkt)/nsend_pkt,'%');
-	close(sockfd);
-	exit(0);
+	long time=(tv_interval.tv_sec *1000)+(tv_interval.tv_usec/1000);
+	printf("---%s ping statistics---\n",dest_str);
+	printf("%d packets transmitted,%d received,%d%c packet loss,time %ld ms.\n",\
+			packet_send,packet_recv,(packet_send-packet_recv)*100/packet_send,'%',time);
 }
 
-int in_chksum(unsigned short *buf,int size)
+
+void icmp_sigint(int signo)
 {
-	int len=size;
-	int sum=0;
-	unsigned short *str=buf;
-	while(len>1)
+	alive=0;
+	gettimeofday(&tv_end,NULL);
+	tv_interval=icmp_tvsub(tv_end,tv_begin);
+}
+
+pingm_packet *icmp_findpacket(int seq)
+{
+	int i=0;
+	pingm_packet *found=NULL;
+	if(seq==-1)   
 	{
-		sum+=*str++;
+		for(i=0;i<128;i++)
+		{
+			if(pingpacket[i].flag==0)
+			{
+				found=&pingpacket[i];
+				break;
+			}
+		}
+	}
+	return found;
+}
+
+
+struct timeval icmp_tvsub(struct timeval end,struct timeval begin)
+{
+	struct timeval tv;
+	tv.tv_sec=end.tv_sec-begin.tv_sec;
+	tv.tv_usec=end.tv_sec-begin.tv_usec;
+
+	if(tv.tv_usec<0)
+	{
+		tv.tv_sec--;
+		tv.tv_usec+=1000000;
+	}
+}
+
+
+
+unsigned short icmp_cksum(unsigned char* data,int len)
+{
+	int sum=0;
+	int old=len & 0x01;  //判断是否为奇数
+	while(len & 0xfffe)  //将数据按照2个字节为单位累加
+	{
+		sum+=*(unsigned short*)data;
+		data+=2;
 		len-=2;
 	}
-	if(len==1)
+
+	if(old)   //如果是奇数，则需要对最后的一个数据进行处理
 	{
-		sum+=*(unsigned char*)str;
+		unsigned short tmp=((*data)<<8)&0xff00;
+		sum+=tmp;
 	}
 
-	// 将32位的高16位与低16位相加
-	sum = (sum >> 16) + (sum & 0xffff);
-	sum += (sum >> 16);
-	return (unsigned char)~sum;
+	sum=(sum>>16)+(sum & 0xffff);  //将高低位相加
+	sum+=(sum>>16);  //将最高位相加
+
+	return ~sum;
 }
 
-int PackIcmp(int send_pkt)
+void icmp_pack(struct icmp *icmp,int seq,struct timeval *tv,int length)
 {
-	struct icmp *pkt=(struct icmp*)icmp_pkt;
-	struct timeval *pTime=NULL;
-	pkt->icmp_type=ICMP_ECHO;
-	pkt->icmp_code=0;
-	pkt->icmp_seq=htons(send_pkt);
-	pkt->icmp_id=getpid();
-	pTime=(struct timeval*)pkt->icmp_data;
-	gettimeofday(pTime,NULL);
-	pkt->icmp_cksum=in_chksum((unsigned short*)pkt,bytes+8);
-	return bytes+8;
-}
-
-double GetRtt(struct timeval *last,struct timeval *first)
-{
-	double rtt;
-	if((last->tv_usec-=first->tv_usec)<0)
+	unsigned char i=0;
+	icmp->icmp_type=ICMP_ECHO;
+	icmp->icmp_code=0;
+	icmp->icmp_cksum=0;
+	icmp->icmp_seq=seq;
+	icmp->icmp_id=pid;
+	for(i=0;i<length;i++)
 	{
-		--last->tv_sec;
-		last->tv_usec+=1000000;
+		icmp->icmp_data[i]=i;
 	}
-	last->tv_sec-=first->tv_sec;
-	rtt=last->tv_sec * 1000 + (double)last->tv_usec/(double)1000;
-	return rtt;
+
+	icmp->icmp_cksum=icmp_cksum((unsigned char*)icmp,length);
 }
 
-int unpack(char *recv_pkt,int size)
+
+int icmp_unpack(char *buf,int len)
 {
+	int i=0;
+	int iphl=0;
 	struct ip *ip=NULL;
-	int iphl;
-	struct icmp *icmp;
-	struct timeval *send;
-	double rtt;
-	
-	ip=(struct ip*)recv_pkt;
-	iphl=ip->ip_hl;  //ip的首部长度为4个字节
-	icmp=(struct icmp*)(recv_pkt+iphl); //越过ip报文的首部，指向icmp报文	
-	size-=iphl;
+	struct icmp *icmp=NULL;
+	int rtt=0;
 
-	if(size<8)
+	ip=(struct ip*)buf;
+	iphl=ip->ip_hl<<2;
+	icmp=(struct icmp*)(buf+iphl);
+	len-=iphl;
+	if(len<8)
 	{
-		printf("ICMP size is less than 8\n");
+		printf("icmp packet length is less than 8\n");
 		return -1;
 	}
-	
-	if((icmp->icmp_type==0) && (icmp->icmp_id=pid)) //判断报文是否是我们发送的
 
+	if((icmp->icmp_type==ICMP_ECHOREPLY)&&(icmp->icmp_id==pid))
 	{
-		send=(struct timeval*)icmp->icmp_data;
-		rtt=GetRtt(&recv_time,send);//计算往返时间
-		printf("%d bytes from %s : icmp_seq=%d ttl=%d time=%.3f ms\n",\
-			size,inet_ntoa(recv_addr.sin_addr),\
-			ntohs(icmp->icmp_seq),ip->ip_ttl,rtt);
+		struct timeval tv_interval;
+		struct timeval tv_send;
+		struct timeval tv_recv;
+
+		pingm_packet *packet=icmp_findpacket(icmp->icmp_seq);
+		if(packet==NULL)
+			return -1;
+
+		packet->flag=0;
+		tv_send=packet->tv_begin;
+
+		gettimeofday(&tv_recv,NULL);
+		tv_interval=icmp_tvsub(tv_recv,tv_send);
+
+		rtt=tv_interval.tv_sec *1000+tv_interval.tv_usec/1000;
+
+		printf("%d byte from %s:icmp_seq=%u ttl=%d rtt=%d ms\n",len,inet_ntoa(ip->ip_src),icmp->icmp_seq,ip->ip_ttl,rtt);
+		packet_recv++;	
 	}
-	return 0;
+	else
+		return -1;
 }
 
-void SendPacket()
+void *icmp_send()
 {
-	int send_bytes=0;
-	int ret=-1;
-	while(1)
+	gettimeofday(&tv_begin,NULL);
+	while(alive)
 	{
-		nsend_pkt++;
-		send_bytes=PackIcmp(nsend_pkt);	
-		printf("socket11:%d\n",sockfd);
-		ret=sendto(sockfd,icmp_pkt,send_bytes,0,\
-				(struct sockaddr*)&dst_addr,sizeof(dst_addr));
-		if(ret==-1)
+		int size=0;
+		struct timeval tv;
+		gettimeofday(&tv,NULL);  //当前包发送的时间
+		pingm_packet *packet=icmp_findpacket(-1);
+		if(packet)
 		{
-			//perror("sendto");
-			printf("send fail\n");
-			sleep(1);
-			//continue;
-			break;
+			packet->seq=packet_send;
+			packet->flag=1;
+			gettimeofday(&packet->tv_begin,NULL);
 		}
+		icmp_pack((struct icmp*)send_buff,packet_send,&tv,64);
+
+		size=sendto(rawsock,send_buff,64,0,(struct sockaddr*)&dest,sizeof(dest));
+		if(size<0)
+		{
+			perror("sendto");
+			continue;
+		}
+		packet_send++;
 		sleep(1);
 	}
 }
 
-void RecevePacket()
+void *icmp_recv()
 {
-	fd_set rd_set;
-	struct timeval time;
-	time.tv_sec=5;
-	time.tv_usec=0;
-	int ret=0;
-	int nread=0;
-	socklen_t recv_len=sizeof(recv_addr);
+	struct timeval tv;
+	tv.tv_usec=200;
+	tv.tv_sec=0;
+	fd_set readfd;
 
-	while(1)
+	while(alive)
 	{
-		FD_ZERO(&rd_set);
-		FD_SET(sockfd,&rd_set);
-		ret=select(sockfd+1,&rd_set,NULL,NULL,&time);
-		if(ret<=0)
+		int ret=0;
+		FD_ZERO(&readfd);
+		FD_SET(rawsock,&readfd);
+		ret=select(rawsock+1,&readfd,NULL,NULL,&tv);
+		switch(ret)
 		{
-			continue;
-		}
-		else if(FD_ISSET(sockfd,&rd_set))
-		{
-			nread=recvfrom(sockfd,recv_pkt,sizeof(recv_pkt),0,\
-					(struct sockaddr*)&recv_addr,&recv_len);
-			if(nread<0)
-			{
-				continue;
-			}
-			gettimeofday(&recv_time,NULL);
+			case -1: //发生错误
+				break;
+			case 0:  //超时
+				break;
+			default:  //收到一个包
+				{
+	
+					int fromlem=0;
+					struct sockaddr from;
 
-			if(unpack(recv_pkt,nread)==-1)
-				continue;
+					int size=recv(rawsock,recv_buff,sizeof(recv_buff),0);
+					if(errno==EINTR)
+					{
+						perror("recv");
+						continue;
+					}
+
+					ret=icmp_unpack(recv_buff,size);
+					if(ret==1)
+						continue;
+				}
 		}
+		break;
 	}
 }
 
-static void usage(char* proc)
+
+static void usage(char *proc)
 {
-		printf("usage:%s[hostname /IP address]\n",proc);
+	printf("%s[hostname|ip adress]",proc);
 }
 
 int main(int argc,char *argv[])
 {
-		if(argc<2)
-		{
-		  usage(argv[0]);
-		  return 1;
-		}
-		
-		struct protoent *protocol=NULL;
-		int ttl=64;
-		struct hostent *host;
-		struct in_addr addr;
-		int name=-1;
-		int size=50*1024;
-		pthread_t send_id,recv_id;
-		protocol=getprotobyname("icmp");
-		if(protocol==NULL)
-		{
-			printf("unknow proto\n");
-			return 2;
-		}
-	   sockfd=socket(AF_INET,SOCK_RAW,protocol->p_proto);
-	   if(sockfd<0)	
-	   {
-		   perror("socket");
-	       return 3;
-	   }
-	setsockopt(sockfd,SOL_SOCKET,SO_RCVBUF,&size,sizeof(size));  //确定缓冲区大小	
-	//setsockopt(sockfd,IPPROTO_IP,IP_MULTICAST_TTL,&ttl,sizeof(ttl));
-	//setsockopt(sockfd,IPPROTO_IP,IP_TTL,&ttl,sizeof(ttl));
-					
-	memset(&dst_addr,0,sizeof(dst_addr));	
-	dst_addr.sin_family=AF_INET;
-	name=inet_aton(argv[1],&addr);
-	if(name==0)  //不是ip地址
+	if(argc!=2)
+	{
+		usage(argv[0]);
+		return 1;
+	}
+	struct hostent *host=NULL;
+	struct protoent *protocol=NULL;
+	int size=128*K;
+	protocol=getprotobyname("icmp");
+	if(protocol==NULL)
+	{
+		perror("getprotobyname");
+		return 2;
+	}
+
+	memcpy(dest_str,argv[1],strlen(argv[1])+1);
+	memset(pingpacket,0,sizeof(pingpacket)*128); //??
+
+	rawsock=socket(AF_INET,SOCK_RAW,protocol->p_proto);
+	if(rawsock<0)
+	{
+		perror("socket");
+		return 3;
+	}
+
+	pid=getuid();
+
+	setsockopt(rawsock,SOL_SOCKET,SO_RCVBUF,&size,sizeof(size)); //增大接收缓冲区
+	bzero(&dest,sizeof(&dest));
+	dest.sin_family=AF_INET;
+
+	int inaddr=inet_addr(argv[1]);
+	if(inaddr==INADDR_NONE)
 	{
 		host=gethostbyname(argv[1]);
 		if(host==NULL)
 		{
-			printf("address is invalid\n");
+			perror("gethostname");
 			return 4;
 		}
-		memcpy(&(dst_addr.sin_addr),host->h_addr,sizeof(struct in_addr));
+		memcpy((char*)&dest.sin_addr,host->h_addr,host->h_length);
 	}
 	else
 	{
-		memcpy(&(dst_addr.sin_addr),&(addr.s_addr),sizeof(struct in_addr));
+		memcpy((char*)&dest.sin_addr,&inaddr,sizeof(inaddr));
 	}
-		pid=getpid();
-		printf("PING %s (%s) %d bytes of data.\n",argv[1],\
-				inet_ntoa(dst_addr.sin_addr),bytes);
-		signal(SIGINT,statistics);
-		printf("socket:%d",sockfd);
-		name=pthread_create(&send_id,NULL,(void*)SendPacket,NULL);
-		printf("sock:%d\n",sockfd);
-		if(name!=0)
-		{
-			perror("pthread_create :send");
-			return 5;
-	   }
-		name=pthread_create(&recv_id,NULL,(void*)RecevePacket,NULL);
-		if(name!=0)
-		{
-			perror("pthread_create :recv");
-			return 6;
-		}	
-		pthread_join(send_id,NULL);
-		pthread_join(recv_id,NULL);
-		return 0;
+
+	inaddr=dest.sin_addr.s_addr;
+	printf("PING %s 56(84) bytes of data.\n",dest_str);
+
+	signal(SIGINT,icmp_sigint);
+
+	alive=1;
+	pthread_t send_id,recv_id;
+	int err=0;
+	err=pthread_create(&send_id,NULL,icmp_send,NULL);
+	if(err<0)
+	{
+		return 5;
+	}
+
+	err=pthread_create(&recv_id,NULL,icmp_recv,NULL);
+	if(err<0)
+	{
+		return 6;
+	}
+
+	pthread_join(send_id,NULL);
+	pthread_join(recv_id,NULL);
+
+	close(rawsock);
+	icmp_statistics();
+	return 0;
 }
--- ping/ping.h
+++ ping/ping.h
@@ -2,44 +2,50 @@
 	> File Name: ping.h
 	> Author: monster
 	> Mail: 1104306242@qq.com 
-	> Created Time: Sun 23 Jul 2017 09:00:21 AM CST
+	> Created Time: Fri 28 Jul 2017 04:19:09 PM CST
  ************************************************************************/
 
-#ifndef __PING_H_
-#define __PING_H_
+#include <sys/socket.h>  
+#include <netinet/in.h>  
+#include <netinet/ip.h>  
+#include <netinet/ip_icmp.h>  
+#include <unistd.h>  
+#include <signal.h>  
+#include <arpa/inet.h>  
+#include <errno.h>  
+#include <sys/time.h>  
+#include <stdio.h>  
+#include <string.h> /* bzero */  
+#include <netdb.h>  
+#include <pthread.h> 
+//保存发送包的状态值  
+typedef struct pingm_pakcet{  
+	struct timeval tv_begin;     //发送时间  
+	struct timeval tv_end;       //接收到的时间  
+	short seq;                   //序列号  
+	int flag;          //1，表示已经发送但是没有接收到回应，0，表示接收到回应  
+}pingm_pakcet;  
+static pingm_pakcet *icmp_findpacket(int seq);  
+static unsigned short icmp_cksum(unsigned char *data, int len);  
+static struct timeval icmp_tvsub(struct timeval end, struct timeval begin);  
+static void icmp_statistics();  
+static void icmp_pack(struct icmp *icmph, int seq, struct timeval *tv,int length);  
+static int icmp_unpack(char *buf,int len);  
+static void *icmp_recv();  
+static void icmp_sigint(int signo);  
+static void icmp_usage();  
 
-#include<stdio.h>
-#include<netinet/ip_icmp.h>
-#include<stdlib.h>
-#include<sys/types.h>
-#include<sys/socket.h>
-#include<unistd.h>
-#include<sys/time.h>
-#include<arpa/inet.h>
-#include<netdb.h>
-#include<netinet/in.h>  
-#include<netinet/ip.h>
-#include<pthread.h>
-#include<string.h>
-#include<signal.h>
+static pingm_pakcet pingpacket[128];  
+#define K 1024  
+#define BUFFERSIZE 72                            //发送缓冲区的大小  
+static unsigned char send_buff[BUFFERSIZE];        
+static unsigned char recv_buff[2*K];             //防止接收溢出，设置大一些  
+static struct sockaddr_in dest;                  //目的地址  
+static int rawsock = 0;                          //发送和接收线程需要的socket描述符  
+static pid_t pid;                                //进程PID  
+static int alive = 0;                            //是否接收到退出信号  
+static short packet_send = 0;                    //已经发送的数据包数量  
+static short packet_recv = 0;                    //已经接收的数据包数量  
+static char dest_str[80];                        //目的主机字符串  
+static struct timeval tv_begin, tv_end, tv_interval; 
 
-struct sockaddr_in dst_addr;
-struct sockaddr_in recv_addr;
-struct timeval recv_time;
-static char icmp_pkt[1024]={0};
-static char recv_pkt[1024]={0};
-
-static int sockfd;
-static int bytes=56;
-static int nsend_pkt=0;
-static int nrecv_pkt=0;
-pid_t pid;
-
-void statistics();
-int in_chksum(unsigned short *buf,int size);
-double GetRtt(struct timeval *last,struct timeval *first);
-int unpack(char *recv_pkt,int size);
-void SendPacket();
-void RecevePacket();
-
-#endif
